1a63108a13d01acdde8de55a1af506991739ec90
Dimitris Kokkonis <kokkonisd@gmail.com>
2022-08-17 14:26:57 +0200

    Update version for next release

--- saul/__init__.py
+++ saul/__init__.py
@@ -1,6 +1,6 @@
 name = "saul"
 description = "A simple license generator, based on choosealicense.com."
-__version_info__ = ("0", "1", "0")
+__version_info__ = ("0", "1", "1")
 __version__ = ".".join(__version_info__)
 __author_name__ = "Dimitri Kokkonis"
 __author_email__ = "kokkonisd@gmail.com"
